1c220bb01a20b7abaaa909e6219d9c2fb7948d09
faisukhan01 <faisukhan01@users.noreply.github.com>
2026-09-01 17:11:21 +0000

    v1.3.0 (versionCode 6)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.fsk.dummysurfers"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.3.0"
     }
 
     signingConfigs {
